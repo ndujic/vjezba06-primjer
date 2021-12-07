b0997e0e9a48f555b76a7f6b80073c87d668eb0a
ndujic <92369601+ndujic@users.noreply.github.com>
2021-12-07 16:00:10 +0100

    Promjena adrese URL na Heroku

--- okviri_p06_frontend/src/services/poruke.js
+++ okviri_p06_frontend/src/services/poruke.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const osnovniUrl = 'http://localhost:3001/api/poruke'
+//const osnovniUrl = 'http://localhost:3001/api/poruke'
+
+const osnovniUrl = 'https://vj06-server-nd.herokuapp.com/api/poruke'
  
 const dohvatiSve = () => {   
     return axios.get(osnovniUrl);
